f9ea490583491f38662a7f792695b8d78ab4a33b
Stephen Appah <appahstephen9@gmail.com>
2025-04-05 16:21:16 +0000

    feat: Done with hero section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { HomeComponent } from "./Pages/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterModule, HomeComponent],
+  imports: [ RouterModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'tantra-hill';
 
-  constructor() {}
+  constructor() { }
 }
